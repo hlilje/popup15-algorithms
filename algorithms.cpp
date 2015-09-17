261fd75b01e593cbddba2abafa2cee7cd1b1a1d4
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-09-17 12:59:42 +0200

    Maybe fix cover algorithm and add test cases

--- algorithms.cpp
+++ algorithms.cpp
@@ -14,30 +14,14 @@ const std::vector<int> cover(const Interval& interval,
 
     std::sort(parts.begin(), parts.end());
 
-    while (min < max)
+    // TODO: Double comparison
+    while ((left_coord < interval._right) && (min < max))
     {
         Interval part = parts[min];
         Interval best_part = part;
         std::cout << "Init best: " << best_part._left << " " << best_part._right << std::endl;
 
-        if (min == max - 1)
-        {
-            if ((part._right < interval._right) || (part._left > left_coord))
-            {
-                indices.clear();
-                break;
-            }
-        }
-
-        // TODO: Double comparison
-        if ((part._right >= interval._right) && (part._left <= left_coord))
-        {
-            if (indices.back() != part._index)
-                indices.push_back(part._index);
-            break;
-        }
-
-        while (part._left <= left_coord)
+        while ((part._left <= left_coord) && (min < max - 1))
         {
             ++min;
             Interval next = parts[min];
@@ -49,9 +33,20 @@ const std::vector<int> cover(const Interval& interval,
             part = next;
         }
 
+        if ((best_part._left > left_coord) || best_part._right <= left_coord)
+            break;
+
         indices.push_back(best_part._index);
         left_coord = best_part._right;
         std::cout << "Pushing: " << best_part._left << " " << best_part._right << std::endl;
+    }
+
+    // Since the last part is always added, verify that it is legit
+    if (max > 0)
+    {
+        Interval last = parts[indices.back()];
+        if ((last._left > left_coord) || (last._right < interval._right))
+            indices.clear();
     }
 
     return indices;
